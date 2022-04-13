b66008f7d53598020e69db62072ea102c08867a1
rishu4614 <sharmagkp00@gmail.com>
2022-04-13 17:14:19 +0530

    1st commit

---
+++ TDD_Code/SerialPortParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Code
+{
+    public class SerialPortParser
+    {
+        public static int ParsePort(string port)
+        {
+            if (!port.StartsWith("COM"))                                // it checks that the Port is in appropriate format
+            {                                                       
+                throw new FormatException("Port is not in a correct format.");  //
+            }
+            else
+            {
+                const int lastIndexOfPrefix = 3;
+                string portNumber = port.Substring(lastIndexOfPrefix);
+                return int.Parse(portNumber);
+            }
+        }
+    }
+}
---
+++ TDD_Test/TDD_Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TDD_Code;
+
+namespace TDD_Test
+{
+    [TestFixture]
+    public class TDD_Test
+    {
+        [Test]
+        public void ParsePort_COM1_Return1()
+        {
+            int result = SerialPortParser.ParsePort("COM567");        // result stores the port_number 
+            Assert.That(result, Is.EqualTo(567));                      // comparing the value of result and port_number(567)
+        }
+
+        [Test]
+        public void ParsePort_InvalidFormat_ThrowInvalidFormatException()
+        {
+            TestDelegate action = () => SerialPortParser.ParsePort("1");       //it throws the invalid format exception
+            Assert.Throws<FormatException>(action);
+        }
+
+    }
+}
