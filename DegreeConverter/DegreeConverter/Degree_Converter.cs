c469fe9d1baad37d0aaefddd9194cd7b0a68341e
rishu4614 <sharmagkp00@gmail.com>
2022-04-13 18:16:22 +0530

    2nd commit

---
+++ DegreeConverter/DegreeConverter/Degree_Converter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegreeConverter
+{
+    public class Degree_Converter
+    {
+        public double ToFehrenhite(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+        public double ToCelsius(double fahrenhite)
+        {
+            return (32 * fahrenhite - 32) * 5 / 9;
+        }
+    }
+}
---
+++ DegreeConverter/Test/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DegreeConverter;
+using NUnit.Framework;
+
+
+namespace Test
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void ToFehrenheite_ZeroCelcius_Return32()
+        {
+            Degree_Converter convert = new Degree_Converter();
+            double result = convert.ToFehrenhite(0);
+            Assert.That(result, Is.EqualTo(32));
+        }
+
+        [Test]
+        public void ToCelcius_1Fahrenhite_Return0()
+        {
+            Degree_Converter convert = new Degree_Converter();
+            double result = convert.ToFehrenhite(1);
+            Assert.That(result, Is.EqualTo(0));
+        }
+    }
+}
