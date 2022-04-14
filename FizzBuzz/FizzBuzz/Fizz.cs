1f6c08732401ce4d149a7ec9ce66c99aa7aa8940
rishu4614 <sharmagkp00@gmail.com>
2022-04-14 13:03:48 +0530

    Fizz_Buzz_Code

---
+++ FizzBuzz/FizzBuzz/Fizz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class Fizz
+    {
+        public static string Ask(int Number)
+        {
+            if (Number % 3 == 0 && Number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (Number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (Number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            return "";
+        }
+    }
+}
---
+++ FizzBuzz/Test/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FizzBuzz;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [TestCase("Fizz", 3)]
+        [TestCase("Buzz", 6)]
+        [TestCase("FizzBuzz", 15)]
+        [TestCase("", 7)]
+        public void Test(string output, int number)
+        {
+            Assert.AreEqual(output, Fizz.Ask(number));
+        }
+    }
+}
