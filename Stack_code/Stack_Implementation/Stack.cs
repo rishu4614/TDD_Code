49d48bcd55e2fe05d2fc94fec32458437fdd9908
rishu4614 <sharmagkp00@gmail.com>
2022-04-14 14:50:38 +0530

    Stack_code

---
+++ Stack_code/Stack_Implementation/Stack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Stack_Implementation
+{
+    public class MyStack<T>
+    {
+        private readonly List<T> list = new List<T>();
+        public bool IsEmpty => Count == 0;
+        public int Count => list.Count;
+
+        public void Push(T value)
+        {
+            list.Add(value);
+        }
+        public void Pop()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException();
+            list.RemoveAt(Count - 1);
+        }
+        public T Peek()
+        {
+            return list[Count - 1];
+        }
+    }
+}
