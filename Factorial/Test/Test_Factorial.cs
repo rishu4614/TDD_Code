7f7b83191cb44d12899888fd2676f5b178fb1305
rishu4614 <sharmagkp00@gmail.com>
2022-04-14 19:19:41 +0530

    Commit Factorial

---
+++ Factorial/Test/Test_Factorial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Factorial;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class Test_Factorial
+    {
+        
+        [TestCase(1, 0)]
+        [TestCase(1, 1)]
+        [TestCase(120, 5)]
+        public void Fact(int output, int number)
+        {
+            var f = new Fact();
+            var result = f.GetFactorial(number);
+            Assert.AreEqual(output, result);
+
+        }
+
+        
+    }
+}
