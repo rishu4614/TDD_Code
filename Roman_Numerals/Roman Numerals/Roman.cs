70fddcbd1d7570efcb3643440ddf60f8d925287a
rishu4614 <sharmagkp00@gmail.com>
2022-04-14 13:49:48 +0530

    Roman_Numerals

---
+++ Roman_Numerals/Roman Numerals/Roman.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roman_Numerals
+{
+    public class Roman
+    {
+        private static readonly Dictionary<char, int> Link = new Dictionary<char, int>()
+        {
+            {'I', 1},
+            {'V',5},
+            {'X',10 },
+            {'L',50 },
+            {'C',1000 },
+            {'D',5000 },
+            {'M',1000 },
+        };
+        public static int Parse(string roman)
+        {
+            int result = 0;
+            for(int i =0; i<roman.Length; ++i)
+            {
+                if(i+1 < roman.Length && IsSubtractive(roman[i], roman[i+1]))
+                {
+                    result -= Link[roman[i]];
+                }
+                else
+                {
+                    result += Link[roman[i]];
+                }
+                
+            }
+            return result;
+        }
+
+        private static bool IsSubtractive(char c1, char c2)
+        {
+            return Link[c1] < Link[c2];
+        }
+    }
+}
---
+++ Roman_Numerals/Test/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Roman_Numerals;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class Test
+    {
+        [TestCase(1, "I")]
+        [TestCase(5, "V")]
+        [TestCase(2, "II")]
+        [TestCase(4, "IV")]
+        public void Parse(int output, string roman)
+        {
+            Assert.AreEqual(output, Roman.Parse(roman));
+        }
+    }
+}
