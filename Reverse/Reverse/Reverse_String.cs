bded8b35e462f1c8cc81e6c9328d5409fec2c41c
rishu4614 <sharmagkp00@gmail.com>
2022-04-14 19:39:55 +0530

    commit Reverse

---
+++ Reverse/Reverse/Reverse_String.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse
+{
+    public class Reverse_String
+    {
+        public char[] Reverse_Me(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return arr;
+        }
+
+    }
+}
---
+++ Reverse/Test/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reverse;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class Test
+    {
+        [TestCase("FEDCBA", "ABCDEF")]
+        [TestCase("WERT", "RISHABH")]
+        public void Test_reverse(string output, string input)
+        {
+            var rev = new Reverse_String();
+            var result = rev.Reverse_Me(input);
+            Assert.AreEqual(output, result);
+        }
+    }
+}
